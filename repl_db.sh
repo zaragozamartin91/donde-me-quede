1d504b99244b90d887dd581723b0d18056981a21
zaragozamartin91 <zaragozamartin91@gmail.com>
2021-04-16 18:46:57 -0300

    improving local postgres repl

--- repl_db.sh
+++ repl_db.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --rm --link dmq-db postgres psql -h dmq-db -U dondemequede -p 5432 dmq
+docker run -it --rm --link dmq-db -e PGPASSWORD=d0nd3m3qu3d3 postgres psql -h dmq-db -U dondemequede -p 5432 dmq
